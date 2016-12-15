33f80b1db60125b03e26b1f2f70a34ccd037cf59
FeodorM <Feodor.Mann@gmail.com>
2016-12-15 16:20:22 +0300

    modify Model3D

--- lab_3/model3d.py
+++ lab_3/model3d.py
@@ -2,20 +2,23 @@ from util.matrix import Matrix
 
 
 class Model3D:
-    def __init__(self, vertices_file: str = None, verges_file: str = None):
-        self.vertices = [[0], [0], [0], [1]]
+    def __init__(self, vertices_file: str = None, verges_file: str = None, world_to_project_matrix: Matrix = None):
+        self.vertices = [[], [], [], []]
         self.verges = []
         self.edges = []
+        self.project_vertices = None
 
         if vertices_file is not None:
             self.load_vertices_from_file(vertices_file)
 
         if verges_file is not None:
             self.load_verges_from_file(verges_file)
+            if world_to_project_matrix is not None:
+                self.calc_project_vertices(world_to_project_matrix)
 
     def __getitem__(self, item):
-        den = 1 / self.vertices[3][item]
-        return self.vertices[0][item] * den, self.vertices[1][item] * den, self.vertices[2][item] * den
+        den = 1 / self.project_vertices[2][item]
+        return self.project_vertices[0][item] * den, self.project_vertices[1][item] * den
 
     @staticmethod
     def line_to_skip(line):
@@ -38,7 +41,7 @@ class Model3D:
         with open(filename) as f:
             for line in f:
                 if not self.line_to_skip(line):
-                    verge = tuple(map(lambda x: int(x) + 1, line.strip().split()))
+                    verge = tuple(map(int, line.strip().split()))
                     self.verges.append(verge)
         self.load_edges_from_verges()
 
@@ -50,8 +53,12 @@ class Model3D:
             edges.add((verge[2], verge[0]))
         self.edges.extend(list(edges))
 
-    def apply(self, a: Matrix):
-        self.vertices = a * self.vertices
+    def calc_project_vertices(self, matrix):
+        self.project_vertices = matrix * self.vertices
+
+    def apply(self, affine_transform: Matrix, world_to_project_matrix: Matrix):
+        self.vertices = affine_transform * self.vertices
+        self.calc_project_vertices(world_to_project_matrix)
 
     @property
     def center(self):
