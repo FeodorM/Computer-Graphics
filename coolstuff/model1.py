423611f0a275d5b5c0fde5046f6d0245f9b406af
FeodorM <Feodor.Mann@gmail.com>
2017-01-03 19:55:15 +0300

    update Model (now Model1(BaseModel))

---
+++ coolstuff/model1.py
@@ -0,0 +1,63 @@
+from math import cos, sin
+
+from coolstuff.base_model import BaseModel
+from util.matrix import Matrix
+
+
+def update_model(func):
+    def wrapped(self, *args):
+        func(self, *args)
+        self._calc_current_vertices()
+    return wrapped
+
+
+class Model1(BaseModel):
+
+    def _init(self):
+        self.x_p = 0
+        self.y_p = 0
+        self.alpha = 0
+        self.kx = 1
+        self.ky = 1
+
+        self.vertices_current = None
+        self._calc_current_vertices()
+
+    def _calc_current_vertices(self):
+        x, y, kx, ky = self.x_p, self.y_p, self.kx, self.ky
+        c = cos(self.alpha)
+        s = sin(self.alpha)
+        self.vertices_current = Matrix([
+            [kx * c, - ky * s, x],
+            [kx * s,   ky * c, y],
+            [0,        0,      1]
+        ]) * self.vertices_initial
+
+    @update_model
+    def translate(self, x, y):
+        self.x_p += x
+        self.y_p += y
+
+    @update_model
+    def rotate(self, alpha):
+        self.alpha += alpha
+
+    @update_model
+    def scale(self, kx, ky=None):
+        if ky is None:
+            ky = kx
+        self.kx *= kx
+        self.ky *= ky
+
+    @update_model
+    def mirror(self):
+        self.kx *= -1
+        self.ky *= -1
+
+    @update_model
+    def mirror_x(self):
+        self.ky *= -1
+
+    @update_model
+    def mirror_y(self):
+        self.kx *= -1
